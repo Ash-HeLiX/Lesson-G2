c30802d6ebfd30c8a14902b59aa5e5284387f8e3
ashelix <41845417+ashelix@users.noreply.github.com>
2018-07-29 08:36:35 +0300

    Add files via upload

---
+++ l4.cpp
@@ -0,0 +1,15 @@
+//Ќапишите программу, вывод€щую на экран ваше любимое стихотворение
+//ƒл€ разбиени€ на строчки используйте подход€щую управл€ющую последовательность.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout 	<< "Some say the world will end in fire,"			// "\n" перебрасывает текст на другую строчку
+			<< "\nSome say in ice."
+			<< "\nFrom what IТve tasted of desire"
+			<< "\nI hold with those who favor fire."
+			<< "\nBut if it had to perish twice,"
+			<< "\nI think I know enough of hate" ;
+}
